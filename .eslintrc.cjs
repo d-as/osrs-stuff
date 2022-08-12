f4c0766195a99bd4b508f0043a009bd7b20cc8ac
d-as <notdasdas@gmail.com>
2022-08-13 02:44:25 +0300

    Setup Vite + React + TypeScript + ESLint

---
+++ .eslintrc.cjs
@@ -0,0 +1,41 @@
+const { OFF, ERROR } = {
+  OFF: 0,
+  WARN: 1,
+  ERROR: 2,
+};
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react/jsx-runtime',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'import/prefer-default-export': OFF,
+    'react/function-component-definition': [ERROR, {
+      namedComponents: 'arrow-function',
+      unnamedComponents: 'arrow-function',
+    }],
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import './App.css';
+
+const increment = (value: number): number => value + 1;
+
+export const App = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>osrs-vite</h1>
+      <div>
+        <button type="button" onClick={() => setCount(increment)}>
+          {`Count is ${count}`}
+        </button>
+      </div>
+    </div>
+  );
+};
